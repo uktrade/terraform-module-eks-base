467da914880e97cce56a9aff701dde6c9c2cd2c5
Bowen Sun <bowensun@gmail.com>
2019-04-10 16:13:49 +0100

    fix multiline string policy

--- 01-base/01-eip-policy.tf
+++ 01-base/01-eip-policy.tf
@@ -2,16 +2,16 @@ resource "aws_iam_role_policy" "eks-node-eip" {
   name = "${var.cluster_id}-node-eip"
   role = "${var.worker_iam_role_name}"
   policy = <<EOF
-    {
-      "Version": "2012-10-17",
-      "Statement": [{
-        "Effect": "Allow",
-        "Action": [
-          "ec2:DisassociateAddress",
-          "ec2:AssociateAddress"
-        ],
-        "Resource": "*"
-      }]
-    }
-  EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [{
+    "Effect": "Allow",
+    "Action": [
+      "ec2:DisassociateAddress",
+      "ec2:AssociateAddress"
+    ],
+    "Resource": "*"
+  }]
+}
+EOF
 }
