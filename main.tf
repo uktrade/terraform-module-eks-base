c5f849058247c352db45485bd1255376761a92ae
Bowen Sun <bowensun@gmail.com>
2019-04-10 17:17:14 +0100

    fix missing var

--- main.tf
+++ main.tf
@@ -2,4 +2,5 @@ module "eks-base" {
   source = "github.com/uktrade/terraform-module-eks-base//base"
   cluster_id = "${var.cluster_id}"
   worker_iam_role_name = "${var.worker_iam_role_name}"
+  kubeconfig_filename = "${var.kubeconfig_filename}"
 }
