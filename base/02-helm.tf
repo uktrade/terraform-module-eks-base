7704edfe1854c0e7a4d7f8ffa63e10fe607c4e72
Bowen Sun <bowensun@gmail.com>
2019-04-11 12:39:38 +0100

    fix helm init

--- base/02-helm.tf
+++ base/02-helm.tf
@@ -28,7 +28,7 @@ resource "kubernetes_cluster_role_binding" "tiller" {
 
 resource "null_resource" "helm_init" {
   provisioner "local-exec" {
-    command = "helm init --upgrade --wait --dry-run --debug"
+    command = "helm init --upgrade --wait"
     environment {
       KUBECONFIG = "${var.kubeconfig_filename}"
     }
