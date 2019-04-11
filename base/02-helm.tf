0ef29973d62ed7b4e54fa224bb3533d4017e7d39
Bowen Sun <bowensun@gmail.com>
2019-04-11 10:52:08 +0100

    default helm repos

--- base/02-helm.tf
+++ base/02-helm.tf
@@ -7,3 +7,13 @@ provider "helm" {
     config_path = "${var.kubeconfig_filename}"
   }
 }
+
+data "helm_repository" "stable" {
+    name = "stable"
+    url = "https://kubernetes-charts.storage.googleapis.com"
+}
+
+data "helm_repository" "incubator" {
+    name = "incubator"
+    url = "https://kubernetes-charts-incubator.storage.googleapis.com"
+}
