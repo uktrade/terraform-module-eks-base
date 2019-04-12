8b93a79e8adebe64470a37cf221cc6ebed51a66a
Bowen Sun <bowensun@gmail.com>
2019-04-12 12:51:18 +0100

    add namespace to nginx-ingress

--- base/05-ingress-controller.tf
+++ base/05-ingress-controller.tf
@@ -1,5 +1,6 @@
 resource "helm_release" "nginx-ingress" {
   name = "nginx-ingress"
+  namespace = "kube-system"
   repository = "{data.helm_repository.stable.metadata.0.name}"
   chart = "nginx-ingress"
   version = "1.4.0"
