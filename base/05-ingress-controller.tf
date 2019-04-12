cbde36b4c2a3730b9aa5f5bfd617e5250200c47f
Bowen Sun <bowensun@gmail.com>
2019-04-12 13:03:16 +0100

    typo

--- base/05-ingress-controller.tf
+++ base/05-ingress-controller.tf
@@ -1,7 +1,7 @@
 resource "helm_release" "nginx-ingress" {
   name = "nginx-ingress"
   namespace = "kube-system"
-  repository = "{data.helm_repository.stable.metadata.0.name}"
+  repository = "${data.helm_repository.stable.metadata.0.name}"
   chart = "nginx-ingress"
   version = "1.4.0"
 }
