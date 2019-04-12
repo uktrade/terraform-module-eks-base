142558273cf28d331d9a13de450c0650cf226d1d
Bowen Sun <bowensun@gmail.com>
2019-04-12 16:38:59 +0100

    IAM policy for nginx-ingress

--- base/05-ingress-controller.tf
+++ base/05-ingress-controller.tf
@@ -1,3 +1,34 @@
+resource "aws_iam_role_policy" "nginx-ingress" {
+  name = "${var.cluster_id}-node-eip"
+  role = "${var.worker_iam_role_name}"
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [{
+    "Effect": "Allow",
+    "Action": [
+      "acm:ListCertificates",
+      "acm:DescribeCertificate",
+      "autoscaling:DescribeAutoScalingGroups",
+      "autoscaling:AttachLoadBalancers",
+      "autoscaling:DetachLoadBalancers",
+      "autoscaling:DetachLoadBalancerTargetGroups",
+      "autoscaling:AttachLoadBalancerTargetGroups",
+      "autoscaling:DescribeLoadBalancerTargetGroups",
+      "elasticloadbalancing:*",
+      "ec2:Describe*",
+      "iam:GetServerCertificate",
+      "iam:ListServerCertificates"
+    ],
+    "Resource": [
+      "*"
+    ]
+  }]
+}
+EOF
+}
+
+
 resource "helm_release" "nginx-ingress" {
   name = "nginx-ingress"
   namespace = "kube-system"
