c882f8d43a93fc5d85a15cadb8a7657e1f59a1e0
Bowen Sun <bowensun@gmail.com>
2019-04-12 11:32:32 +0100

    fix local vars

--- base/03-sdn.tf
+++ base/03-sdn.tf
@@ -5,7 +5,7 @@ locals {
 
 resource "null_resource" "k8s-cni" {
   provisioner "local-exec" {
-    command = "kubectl apply -f ${amazon-k8s-cni-url}/aws-k8s-cni.yaml"
+    command = "kubectl apply -f ${local.amazon-k8s-cni-url}/aws-k8s-cni.yaml"
     environment {
       KUBECONFIG = "${var.kubeconfig_filename}"
     }
@@ -14,7 +14,7 @@ resource "null_resource" "k8s-cni" {
 
 resource "null_resource" "k8s-calico" {
   provisioner "local-exec" {
-    command = "kubectl apply -f ${amazon-k8s-cni-url}/calico.yaml"
+    command = "kubectl apply -f ${local.amazon-k8s-cni-url}/calico.yaml"
     environment {
       KUBECONFIG = "${var.kubeconfig_filename}"
     }
