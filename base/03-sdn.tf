872db7983c623b7f4d20b0b622d26c9b4bc4b9e9
Bowen Sun <bowensun@gmail.com>
2019-05-07 16:50:26 +0100

    bump SDN version and add metrics

--- base/03-sdn.tf
+++ base/03-sdn.tf
@@ -1,5 +1,5 @@
 locals {
-  amazon-k8s-cni-release = "1.3"
+  amazon-k8s-cni-release = "1.4"
   amazon-k8s-cni-url = "https://raw.githubusercontent.com/aws/amazon-vpc-cni-k8s/master/config/v${local.amazon-k8s-cni-release}"
 }
 
@@ -15,6 +15,15 @@ resource "null_resource" "k8s-cni" {
 resource "null_resource" "k8s-calico" {
   provisioner "local-exec" {
     command = "kubectl apply -f ${local.amazon-k8s-cni-url}/calico.yaml"
+    environment {
+      KUBECONFIG = "${var.kubeconfig_filename}"
+    }
+  }
+}
+
+resource "null_resource" "k8s-calico-metrics" {
+  provisioner "local-exec" {
+    command = "kubectl apply -f ${local.amazon-k8s-cni-url}/cni-metrics-helper.yaml"
     environment {
       KUBECONFIG = "${var.kubeconfig_filename}"
     }
