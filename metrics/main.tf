004ff7ab9a0e6eda2e743c515f6bf03a60883e44
Bowen Sun <bowensun@gmail.com>
2019-05-21 15:00:53 +0100

    fix prometheus issues

--- metrics/main.tf
+++ metrics/main.tf
@@ -15,6 +15,56 @@ resource "null_resource" "prometheus-init" {
   }
 }
 
+data "template_file" "prometheus-patch_1" {
+  template = <<EOF
+rules:
+- apiGroups:
+  - monitoring.coreos.com
+  resources:
+  - alertmanagers
+  - prometheuses
+  - servicemonitors
+  - prometheusrules
+  verbs:
+  - "*"
+EOF
+}
+
+resource "null_resource" "prometheus-patch_1" {
+  provisioner "local-exec" {
+    command = <<EOF
+cat <<EOL | kubectl patch clusterrole prometheus-operator -p '${data.template_file.prometheus-patch_1.rendered}'
+EOL
+EOF
+    environment {
+      KUBECONFIG = "${var.kubeconfig_filename}"
+    }
+  }
+}
+
+data "template_file" "prometheus-patch_2" {
+  template = <<EOF
+spec:
+  template:
+    spec:
+      containers:
+      - name: prometheus-operator
+        image: quay.io/coreos/prometheus-operator:v0.30.0
+EOF
+}
+
+resource "null_resource" "prometheus-patch_2" {
+  provisioner "local-exec" {
+    command = <<EOF
+cat <<EOL | kubectl patch -n monitoring deployment prometheus-operator -p '${data.template_file.prometheus-patch_2.rendered}'
+EOL
+EOF
+    environment {
+      KUBECONFIG = "${var.kubeconfig_filename}"
+    }
+  }
+}
+
 resource "null_resource" "prometheus" {
   provisioner "local-exec" {
     command = "kubectl apply -f ${local.metrics_url}/prometheus.yaml"
