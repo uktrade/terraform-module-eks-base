8c10a2fea63abeb100062383374a504311e70176
Bowen Sun <bowensun@gmail.com>
2019-05-21 13:29:16 +0100

    remove heapter config not needed

---
+++ metrics/variables.tf
@@ -0,0 +1,15 @@
+variable "cluster_id" {
+  default = ""
+}
+
+variable "cluster_domain" {
+  default = ""
+}
+
+variable "worker_iam_role_name" {
+ default = ""
+}
+
+variable "kubeconfig_filename" {
+  default = ""
+}
