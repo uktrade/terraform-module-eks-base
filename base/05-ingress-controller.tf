68b2afe3f2282423cd4dc2587de682a667ce0087
Bowen Sun <bowensun@gmail.com>
2019-06-06 12:23:23 +0100

    multiple nginx-ingress controllers

--- base/05-ingress-controller.tf
+++ base/05-ingress-controller.tf
@@ -58,8 +58,10 @@ controller:
       service.beta.kubernetes.io/aws-load-balancer-ssl-cert: ${data.aws_acm_certificate.eks-acm.arn}
       service.beta.kubernetes.io/aws-load-balancer-ssl-negotiation-policy: ELBSecurityPolicy-TLS-1-2-2017-01
       service.beta.kubernetes.io/aws-load-balancer-ssl-ports: https
+      service.beta.kubernetes.io/aws-load-balancer-cross-zone-load-balancing-enabled: "true"
   config:
     server-tokens: "false"
+    ssl-redirect: "true"
     use-forwarded-headers: "true"
     use-proxy-protocol: "false"
     enable-vts-status: "true"
@@ -86,4 +88,47 @@ resource "helm_release" "nginx-ingress" {
   repository = "${data.helm_repository.stable.metadata.0.name}"
   chart = "nginx-ingress"
   values = ["${data.template_file.nginx-ingress-values.rendered}"]
+}
+
+data "template_file" "nginx-ingress-external-values" {
+  template = <<EOF
+controller:
+  service:
+    annotations:
+      service.beta.kubernetes.io/aws-load-balancer-backend-protocol: http
+      service.beta.kubernetes.io/aws-load-balancer-connection-idle-timeout: "60"
+      service.beta.kubernetes.io/aws-load-balancer-ssl-cert: ${data.aws_acm_certificate.eks-acm.arn}
+      service.beta.kubernetes.io/aws-load-balancer-ssl-negotiation-policy: ELBSecurityPolicy-TLS-1-2-2017-01
+      service.beta.kubernetes.io/aws-load-balancer-ssl-ports: https
+      service.beta.kubernetes.io/aws-load-balancer-cross-zone-load-balancing-enabled: "true"
+  config:
+    server-tokens: "false"
+    ssl-redirect: "true"
+    use-forwarded-headers: "true"
+    use-proxy-protocol: "false"
+    enable-vts-status: "true"
+    proxy-real-ip-cidr: 0.0.0.0/0
+    proxy-body-size: 5G
+  ingressClass: nginx-external
+  extraArgs:
+    tcp-services-configmap: $(POD_NAMESPACE)/tcp-services
+    udp-services-configmap: $(POD_NAMESPACE)/udp-services
+    annotations-prefix: nginx.ingress.kubernetes.io
+  publishService:
+    enabled: true
+defaultBackend:
+  enabled: false
+stats:
+  enabled: true
+metrics:
+  enabled: true
+EOF
+}
+
+resource "helm_release" "nginx-ingress-external" {
+  name = "nginx-ingress-external"
+  namespace = "kube-system"
+  repository = "${data.helm_repository.stable.metadata.0.name}"
+  chart = "nginx-ingress"
+  values = ["${data.template_file.nginx-ingress-external-values.rendered}"]
 }
