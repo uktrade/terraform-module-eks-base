1c3d8c0f5804f529a65f0ebce7253150e4837267
Bowen Sun <bowensun@gmail.com>
2019-10-24 11:31:14 +0100

    add IAM policy for aws-ebs-csi driver

--- base/01-iam-policy.tf
+++ base/01-iam-policy.tf
@@ -44,3 +44,33 @@ resource "aws_iam_role_policy" "k8s-autoscaler" {
 }
 EOF
 }
+
+resource "aws_iam_role_policy" "k8s-csi" {
+  name = "${var.cluster_id}-k8s-csi"
+  role = "${var.worker_iam_role_name}"
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "ec2:AttachVolume",
+        "ec2:CreateSnapshot",
+        "ec2:CreateTags",
+        "ec2:CreateVolume",
+        "ec2:DeleteSnapshot",
+        "ec2:DeleteTags",
+        "ec2:DeleteVolume",
+        "ec2:DescribeInstances",
+        "ec2:DescribeSnapshots",
+        "ec2:DescribeTags",
+        "ec2:DescribeVolumes",
+        "ec2:DetachVolume"
+      ],
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
