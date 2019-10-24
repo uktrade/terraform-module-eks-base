c837714541dfce2ba2a59622823f72d4cb096e77
Bowen Sun <bowensun@gmail.com>
2019-10-24 13:18:28 +0100

    remove features not supported in EKS - https://aws.amazon.com/blogs/opensource/eks-support-ebs-csi-driver/ - https://github.com/kubernetes-sigs/aws-ebs-csi-driver/issues/387

--- base/08-csi.tf
+++ base/08-csi.tf
@@ -7,18 +7,6 @@ resource "helm_release" "aws-ebs-csi" {
   namespace = "kube-system"
   chart = "${local.aws-ebs-csi-driver-url}"
   version = "${var.helm_release["aws-ebs-csi-driver"]}"
-  set {
-    name = "enableVolumeScheduling"
-    value = "true"
-  }
-  set {
-    name = "enableVolumeResizing"
-    value = "true"
-  }
-  set {
-    name = "enableVolumeSnapshot"
-    value = "true"
-  }
 }
 
 data "template_file" "aws-ebs-storage-class" {
@@ -29,13 +17,6 @@ metadata:
   name: ebs
 provisioner: ebs.csi.aws.com
 volumeBindingMode: WaitForFirstConsumer
-allowVolumeExpansion: true
----
-apiVersion: snapshot.storage.k8s.io/v1alpha1
-kind: VolumeSnapshotClass
-metadata:
-  name: ebs-snapshot
-snapshotter: ebs.csi.aws.com
 EOF
 }
 
