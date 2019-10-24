6f5ad851bc11532be3be6c402e56dfe6cf254a33
Bowen Sun <bowensun@gmail.com>
2019-10-24 12:17:38 +0100

    add aws-ebs-csi storage class

--- base/08-csi.tf
+++ base/08-csi.tf
@@ -20,3 +20,37 @@ resource "helm_release" "aws-ebs-csi" {
     value = "true"
   }
 }
+
+data "template_file" "aws-ebs-storage-class" {
+  template = <<EOF
+kind: StorageClass
+apiVersion: storage.k8s.io/v1
+metadata:
+  name: ebs
+provisioner: ebs.csi.aws.com
+volumeBindingMode: WaitForFirstConsumer
+allowVolumeExpansion: true
+---
+apiVersion: snapshot.storage.k8s.io/v1alpha1
+kind: VolumeSnapshotClass
+metadata:
+name: ebs-snapshot
+snapshotter: ebs.csi.aws.com
+EOF
+}
+
+resource "null_resource" "aws-ebs-storage-class" {
+  provisioner "local-exec" {
+    command = <<EOF
+cat <<EOL | kubectl -n kube-system apply -f -
+${data.template_file.aws-ebs-storage-class.rendered}
+EOL
+EOF
+    environment {
+      KUBECONFIG = "${var.kubeconfig_filename}"
+    }
+  }
+  triggers {
+    build_number = "${sha1(data.template_file.aws-ebs-storage-class.rendered)}"
+  }
+}
