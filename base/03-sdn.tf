ef10d65c51e5965968f2159d2b33fbdd304bc5fb
Bowen Sun <bowensun@gmail.com>
2019-11-07 12:09:28 +0000

    disable manual install of AWS CNI

--- base/03-sdn.tf
+++ base/03-sdn.tf
@@ -4,21 +4,21 @@ locals {
   amazon-k8s-cni-url = "https://raw.githubusercontent.com/aws/amazon-vpc-cni-k8s/v${local.amazon-k8s-cni-version}/config/v${local.amazon-k8s-cni-release}"
 }
 
-data "http" "k8s-cni" {
-  url = "${local.amazon-k8s-cni-url}/aws-k8s-cni.yaml"
-}
-
-resource "null_resource" "k8s-cni" {
-  provisioner "local-exec" {
-    command = "kubectl apply -f ${local.amazon-k8s-cni-url}/aws-k8s-cni.yaml"
-    environment {
-      KUBECONFIG = "${var.kubeconfig_filename}"
-    }
-  }
-  triggers {
-    build_number = "${sha1(data.http.k8s-cni.body)}"
-  }
-}
+# data "http" "k8s-cni" {
+#   url = "${local.amazon-k8s-cni-url}/aws-k8s-cni.yaml"
+# }
+#
+# resource "null_resource" "k8s-cni" {
+#   provisioner "local-exec" {
+#     command = "kubectl apply -f ${local.amazon-k8s-cni-url}/aws-k8s-cni.yaml"
+#     environment {
+#       KUBECONFIG = "${var.kubeconfig_filename}"
+#     }
+#   }
+#   triggers {
+#     build_number = "${sha1(data.http.k8s-cni.body)}"
+#   }
+# }
 
 # data "template_file" "k8s-cni-patch" {
 #   template = <<EOF
