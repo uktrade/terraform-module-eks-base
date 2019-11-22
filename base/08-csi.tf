09af6400f933928abcff4ee37f19caba5093254c
Bowen Sun <bowensun@gmail.com>
2019-11-22 10:39:02 +0000

    re-enable CSI for EKS 1.14

--- base/08-csi.tf
+++ base/08-csi.tf
@@ -2,12 +2,12 @@ locals {
   aws-ebs-csi-driver-url = "https://github.com/kubernetes-sigs/aws-ebs-csi-driver/releases/download/v${var.helm_release["aws-ebs-csi-driver"]}/helm-chart.tgz"
 }
 
-# resource "helm_release" "aws-ebs-csi" {
-#   name = "aws-ebs-csi-driver"
-#   namespace = "kube-system"
-#   chart = local.aws-ebs-csi-driver-url
-#   version = var.helm_release["aws-ebs-csi-driver"]
-# }
+resource "helm_release" "aws-ebs-csi" {
+  name = "aws-ebs-csi-driver"
+  namespace = "kube-system"
+  chart = local.aws-ebs-csi-driver-url
+  version = var.helm_release["aws-ebs-csi-driver"]
+}
 
 data "template_file" "aws-ebs-storage-class" {
   template = <<EOF
@@ -20,18 +20,18 @@ volumeBindingMode: WaitForFirstConsumer
 EOF
 }
 
-# resource "null_resource" "aws-ebs-storage-class" {
-#   provisioner "local-exec" {
-#     command = <<EOF
-# cat <<EOL | kubectl -n kube-system apply -f -
-# ${data.template_file.aws-ebs-storage-class.rendered}
-# EOL
-# EOF
-#     environment = {
-#       KUBECONFIG = var.kubeconfig_filename
-#     }
-#   }
-#   triggers = {
-#     build_number = sha1(data.template_file.aws-ebs-storage-class.rendered)
-#   }
-# }
+resource "null_resource" "aws-ebs-storage-class" {
+  provisioner "local-exec" {
+    command = <<EOF
+cat <<EOL | kubectl -n kube-system apply -f -
+${data.template_file.aws-ebs-storage-class.rendered}
+EOL
+EOF
+    environment = {
+      KUBECONFIG = var.kubeconfig_filename
+    }
+  }
+  triggers = {
+    build_number = sha1(data.template_file.aws-ebs-storage-class.rendered)
+  }
+}
