56412cac79cc2c0f7c36b1b52faa5e89a435a3b6
Bowen Sun <bowensun@gmail.com>
2019-11-27 14:30:47 +0000

    aws specific config for cluster autoscaler

--- base/07-autoscaler.tf
+++ base/07-autoscaler.tf
@@ -4,6 +4,14 @@ cloudProvider: aws
 awsRegion: ${data.aws_region.current.name}
 autoDiscovery:
   clusterName: ${var.cluster_id}
+podAnnotations:
+  cluster-autoscaler.kubernetes.io/safe-to-evict: "false"
+extraArgs:
+  v: 4
+  stderrthreshold: info
+  logtostderr: true
+  balance-similar-node-groups: true
+  skip-nodes-with-system-pods: false
 sslCertHostPath: /etc/ssl/certs/ca-bundle.crt
 rbac:
   create: true
