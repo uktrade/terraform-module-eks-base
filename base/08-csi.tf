4c17eb204ca4b2545bdebc22a59dd61c4d43a852
Bowen Sun <bowensun@gmail.com>
2020-03-24 14:32:29 +0000

    add aws-efs-csi

--- base/08-csi.tf
+++ base/08-csi.tf
@@ -1,5 +1,6 @@
 locals {
   aws-ebs-csi-driver-url = "https://github.com/kubernetes-sigs/aws-ebs-csi-driver/releases/download/v${var.helm_release["aws-ebs-csi-driver"]}/helm-chart.tgz"
+  aws-efs-csi-driver-url = "https://github.com/kubernetes-sigs/aws-efs-csi-driver/releases/download/v${var.helm_release["aws-efs-csi-driver"]}/helm-chart.tgz"
 }
 
 resource "helm_release" "aws-ebs-csi" {
@@ -15,4 +16,25 @@ resource "kubernetes_storage_class" "aws-ebs-storage-class" {
   }
   storage_provisioner = "ebs.csi.aws.com"
   volume_binding_mode = "WaitForFirstConsumer"
+  parameters {
+    encrypted = "true"
+  }
+}
+
+resource "helm_release" "aws-efs-csi" {
+  name = "aws-efs-csi-driver"
+  namespace = "kube-system"
+  chart = local.aws-efs-csi-driver-url
+  version = var.helm_release["aws-efs-csi-driver"]
+}
+
+resource "kubernetes_storage_class" "aws-efs-storage-class" {
+  metadata {
+    name = "efs"
+  }
+  storage_provisioner = "efs.csi.aws.com"
+  volume_binding_mode = "WaitForFirstConsumer"
+  parameters {
+    encrypted = "true"
+  }
 }
