2bec581b8437e0d7ad0fb08a8dfe42ac55a70b8b
Bowen Sun <bowensun@gmail.com>
2020-11-03 11:56:48 +0000

    update helm repo

--- base/02-helm.tf
+++ base/02-helm.tf
@@ -7,8 +7,8 @@ provider "helm" {
 resource "null_resource" "helm_update" {
   provisioner "local-exec" {
     command = <<EOT
-helm repo add stable https://kubernetes-charts.storage.googleapis.com || true &&
-helm repo add incubator https://kubernetes-charts-incubator.storage.googleapis.com || true &&
+helm repo add stable https://charts.helm.sh/stable || true &&
+helm repo add incubator https://charts.helm.sh/incubator || true &&
 helm repo add bitnami https://charts.bitnami.com/bitnami || true &&
 helm repo add funkypenguin-kubernetes-dashboard https://funkypenguin.github.io/helm-kubernetes-dashboard || true &&
 helm repo add kiwigrid https://kiwigrid.github.io || true &&
