c911e54d5d29cbc972229a393fcda71d67739d2d
Bowen Sun <bowensun@gmail.com>
2021-02-09 16:23:36 +0000

    migrate cluster-autoscaler away from helm deprecated repo

--- base/07-autoscaler.tf
+++ base/07-autoscaler.tf
@@ -11,25 +11,19 @@ autoDiscovery:
 podAnnotations:
   cluster-autoscaler.kubernetes.io/safe-to-evict: "false"
 extraArgs:
-  v: 4
-  stderrthreshold: info
-  logtostderr: true
   balance-similar-node-groups: true
   skip-nodes-with-system-pods: false
-sslCertHostPath: /etc/ssl/certs/ca-bundle.crt
 rbac:
   create: true
 serviceMonitor:
   enabled: true
-nodeSelector:
-  role: worker
 EOF
 }
 
 resource "helm_release" "autoscaler" {
   name = "cluster-autoscaler"
   namespace = "kube-system"
-  repository = "https://charts.helm.sh/stable"
+  repository = "https://kubernetes.github.io/autoscaler"
   chart = "cluster-autoscaler"
   version = var.helm_release["cluster-autoscaler"]
   values = [data.template_file.autoscaler.rendered]
